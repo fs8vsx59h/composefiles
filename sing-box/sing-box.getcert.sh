ce18d6a43bcd53c730f5bb45996d135ad279e780
Collins Scott <106460965+fs8vsx59h@users.noreply.github.com>
2024-03-21 13:36:59 +0800

    Delete sing-box directory

--- sing-box/sing-box.getcert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-touch /etc/sing-box/fullchain.cer
-touch /etc/sing-box/private.key
-jq -r  '.gtscf.Certificates[0].certificate' /etc/traefik/acme.json | base64 -d > /etc/sing-box/fullchain.cer
-jq -r  '.gtscf.Certificates[0].key' /etc/traefik/acme.json | base64 -d > /etc/sing-box/private.key
-sing-box run -c /etc/sing-box/config.json
